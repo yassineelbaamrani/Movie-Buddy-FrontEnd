4de82fae6dbaad3846d36dc287c7527474661347
ryan-mcomber <ryanmcom@hotmail.com>
2021-10-11 11:14:44 -0600

    delete files

--- recommendation_ scripts/FIFA-22-CM-Cheat-Table-main/FIFA-22-CM-Cheat-Table-main/lua/main.lua
+++
@@ -1,8 +0,0 @@
-local TableManager = require 'lua/imports/CheatTableManager';
-
--- assigned in TableManager:start()
-DEBUG_MODE = false 
-
-gCTManager = TableManager:new()
-gCTManager:initialize()
-gCTManager:start()
