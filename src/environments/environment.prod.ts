ff158ce72759cbf75515195099b0a177c3d95a71
Yassine Elbaamrani <yelbaamrani@hotmail.com>
2021-10-13 19:53:44 -0400

    Fixed buttons to redirect to pages

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true
-};
-
-
-export const awsUrl = `http://api-env.eba-udukpxjr.us-east-2.elasticbeanstalk.com/api`
-export const localUrl = `http://localhost:5000/api`
