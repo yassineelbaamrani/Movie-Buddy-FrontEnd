efe5b07f171b14ee761e91391822e7344cbd7368
ryan-mcomber <ryanmcom@hotmail.com>
2021-10-15 20:27:37 -0600

    Button to return to main, movie service made

--- src/app/components/recommend/recommend.component.ts
+++ src/app/components/recommend/recommend.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-recommend',
@@ -7,13 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RecommendComponent implements OnInit {
-  user_id= localStorage.getItem("user-id");
-  
-  constructor() { }
-
+  user_id = localStorage.getItem("user-id");
+  constructor( private router:Router) { }
   ngOnInit(): void {
-    console.log("user id "+this.user_id);
-    
+    function renderHTML(data: any) {
+      for (var i = 0; i < data.results.length; i++) {
+        var imag = new Image();
+        let imgString = "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
+        imag.src = imgString;
+        imag.setAttribute("height", '300');
+
+
+        //this.title.appendChild(imag);
+
+        // movieTitle.innerHTML += "<br />" + "ID: " + data.results[i].id + "<br />" + "Title: " + data.results[i].original_title + "<br />"
+        //     + "Synopsis: " + data.results[i].overview + "<br />" + "<br />";
+
+        var btnn = document.createElement('button');
+        btnn.innerHTML = "Add to my List!";
+        // document.getElementById('title').appendChild(btnn);
+
+        // movieTitle.innerHTML += "<hr />" + "<br />";
+      }
+    }
+
+
   }
 
+  public goToMain() {
+
+
+    this.router.navigate(['/main']);
+  }
 }
