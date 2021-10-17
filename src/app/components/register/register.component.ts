7c613cc0190ffbc21f4e51336d9a03ed13cddd14
Yassine Elbaamrani <yelbaamrani@hotmail.com>
2021-10-17 14:15:54 -0400

    change font color fix something I dont remember

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
-  
+
 import { ClientMessage } from './../../models/client-message';
 import { UserService } from './../../services/user.service';
-import { User} from './../../models/user';
+import { User } from './../../models/user';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -15,25 +15,28 @@ export class RegisterComponent {
   title = 'Register User';
 
   // public, private, protected
-  public user = new User(0,'', '', '', '', '')
+  public user = new User(0, '', '', '', '', '')
   //public address = new Address('', '', '', '')
   public clientMessage = new ClientMessage('');
 
   // inject the UserService into this component
-  constructor(private userService: UserService, private router:Router) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   public registerUser(): void {
-  
+
     // push the address object captured into the User's address's []
-   // this.user.addresses.push(this.address);
+    // this.user.addresses.push(this.address);
 
     // call this.userService.registerUser() method and post it
     this.userService.registerUser(this.user)
       .subscribe( // subscribe to the data returned and do something like generate client message
-        data => {this.clientMessage.message = `Successfully registered ${data.firstName}`, this.router.navigate(['/login']);},   // console.log(`successfully added ${data.firstName}`)
+        data => {
+          this.clientMessage.message = `Successfully registered ${data.firstName}`,
+          this.router.navigate(['/login']);
+        },   // console.log(`successfully added ${data.firstName}`)
         error => this.clientMessage.message = `Something went wrong. Error: ${error}` // console.error(`We got an error: ${error}` 
       )
-    
+
   }
 
 }
