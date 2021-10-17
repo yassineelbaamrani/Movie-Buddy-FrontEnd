d5dcd4d303f9b458c954f60f4090f01bc0b1f2ee
Yassine Elbaamrani <yelbaamrani@hotmail.com>
2021-10-17 19:33:57 -0400

    re added img

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -12,7 +12,7 @@ export class MainComponent implements OnInit {
   user_id = localStorage.getItem("user-id") || '{}';
   user_id_num: number = +this.user_id;
   title = 'MovieBuddy';
-  //image = 'https://mpng.subpng.com/20180211/exw/kisspng-photographic-film-movie-projector-cinema-vintage-retro-movie-projector-silhouette-assignmen-5a80f80fa2ff89.7202684015184015516677.jpg'
+  image = 'https://www.nicepng.com/png/full/33-336008_movie-camera-png-old-video-camera-png.png'
 
   constructor(
     private router: Router,
